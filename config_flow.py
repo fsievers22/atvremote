86df316bf0d800d60658cd2b51b04c55a8d8dabf
Fabian Sievers <fabian.sievers@outlook.com>
2022-12-26 10:30:17 +0000

    Update to current

--- config_flow.py
+++ config_flow.py
@@ -10,6 +10,7 @@ from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
+from atvremote.atvremote import ATVRemote
 
 from .const import DOMAIN
 
@@ -19,58 +20,25 @@ _LOGGER = logging.getLogger(__name__)
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required("host"): str,
-        vol.Required("username"): str,
-        vol.Required("password"): str,
     }
 )
 
-
-class PlaceholderHub:
-    """Placeholder class to make tests pass.
-
-    TODO Remove this placeholder class and replace with things from your PyPI package.
-    """
-
-    def __init__(self, host: str) -> None:
-        """Initialize."""
-        self.host = host
-
-    async def authenticate(self, username: str, password: str) -> bool:
-        """Test if we can authenticate with the host."""
-        return True
-
-
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
-    """Validate the user input allows us to connect.
-
-    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
-    """
-    # TODO validate the data can be used to set up a connection.
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
-
-    hub = PlaceholderHub(data["host"])
-
-    if not await hub.authenticate(data["username"], data["password"]):
-        raise InvalidAuth
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-
-    # Return info that you want to store in the config entry.
-    return {"title": "Name of the device"}
+STEP_USER_DATA_SCHEMA_CODE = vol.Schema(
+    {
+        vol.Required("code"): str,
+    }
+)
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for AndroidTV remote."""
 
     VERSION = 1
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.host: str = None
+        self.atvremote: ATVRemote = None
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -83,21 +51,26 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         errors = {}
 
-        try:
-            info = await validate_input(self.hass, user_input)
-        except CannotConnect:
-            errors["base"] = "cannot_connect"
-        except InvalidAuth:
+        if host := user_input.get("host") is not None:
+            self.host = host
+            self.atvremote = ATVRemote(host)
+            if not await self.atvremote.start_pairing():
+                errors["base"] = "cannot_connect"
+                return self.async_show_form(
+                    step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+                )
+            await self.async_set_unique_id(self.atvremote.unique_id)
+            self._abort_if_unique_id_configured()
+            return self.async_show_form(
+                    step_id="user", data_schema=STEP_USER_DATA_SCHEMA_CODE, errors=errors
+                )
+        if not await self.atvremote.finish_pairing(user_input["code"]):
             errors["base"] = "invalid_auth"
-        except Exception:  # pylint: disable=broad-except
-            _LOGGER.exception("Unexpected exception")
-            errors["base"] = "unknown"
-        else:
-            return self.async_create_entry(title=info["title"], data=user_input)
+            return self.async_show_form(
+                    step_id="user", data_schema=STEP_USER_DATA_SCHEMA_CODE, errors=errors
+                )
+        return self.async_create_entry(title="AndroidTV remote", data={"host": self.host})
 
-        return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
-        )
 
 
 class CannotConnect(HomeAssistantError):
